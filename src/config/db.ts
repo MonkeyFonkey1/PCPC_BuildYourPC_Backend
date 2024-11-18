cc61f6d3a40b686184c5079e4b3b9bf1a16b293c
MonkeyFonkey <andrevirlan@yahoo.com>
2024-11-18 22:23:04 +0200

    connected database, created the models + tested

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        console.log('Connecting to MongoDB with URI:', process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI || '');
+        console.log('MongoDB connected...');
+    } catch (error) {
+        console.error('Database connection error:', error);
+        process.exit(1); // Exit process if connection fails
+    }
+};
+
+export default connectDB;
---
+++ src/models/cachedQuery.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface ICachedQuery extends Document {
+    query_type: string;
+    query_params: Record<string, any>;
+    results: Record<string, any>[];
+    timestamp: Date;
+}
+
+const CachedQuerySchema: Schema = new Schema({
+    query_type: { type: String, required: true },
+    query_params: { type: Object, required: true },
+    results: { type: Array, required: true },
+    timestamp: { type: Date, default: Date.now },
+});
+
+export default mongoose.model<ICachedQuery>('cached_queries', CachedQuerySchema);
